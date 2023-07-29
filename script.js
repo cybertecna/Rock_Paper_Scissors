cfd879d41cb4b01247bc964dc5b873de99555deb
xcodematt <mjkopacki@gmail.com>
2023-07-29 14:57:11 +0200

    Added a counting system and created a function that plays the game 5 times, counts the points and displays who won at the end

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ function getComputerChoice() {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
+let playerScore = 0 
+let computerScore = 0 
 
 function playRound(playerSelection, computerSelection) {
+   
     let victory = "You win!"
     let lose = "You lose!"
     let paperWins = "Paper beats rock!"
@@ -29,16 +32,22 @@ function playRound(playerSelection, computerSelection) {
     playerSelection.toLowerCase()
 
     if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1; 
         return lose + " " + paperWins
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
         return victory + " " + rockWins
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
         return victory + " " + paperWins
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
         return lose + " " + scissorsWin
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
         return lose + " " + rockWins
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
         return victory + " " + scissorsWin
     } else {
         return tie
@@ -46,4 +55,45 @@ function playRound(playerSelection, computerSelection) {
 
   }
    
-  console.log(playRound(playerSelection, computerSelection));
+//   console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+console.log(playRound(playerSelection, computerSelection))
+
+console.log(playerScore)
+console.log(computerScore)
+
+console.log(playRound(playerSelection, computerSelection))
+
+console.log(playerScore)
+console.log(computerScore)
+
+console.log(playRound(playerSelection, computerSelection))
+
+console.log(playerScore)
+console.log(computerScore)
+
+console.log(playRound(playerSelection, computerSelection))
+
+console.log(playerScore)
+console.log(computerScore)
+
+console.log(playRound(playerSelection, computerSelection))
+
+console.log(playerScore)
+console.log(computerScore)
+let youLose = "You lose!"
+let youWin = "You win!"
+let tie = "It's a tie!"
+
+if (playerScore < computerScore) {
+    return youLose
+} else if (playerScore > computerScore) {
+    return youWin
+} else { 
+    return tie
+}
+
+}
+
+console.log(game())
